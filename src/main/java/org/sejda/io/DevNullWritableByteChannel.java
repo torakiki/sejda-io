49ac6b08ca359e3f52db5fc5f8986cd2dcde5aaf
torakiki <andrea.vacondio@gmail.com>
2018-09-13 18:01:41 +0200

    fixed files header with correct license

--- src/main/java/org/sejda/io/DevNullWritableByteChannel.java
+++ src/main/java/org/sejda/io/DevNullWritableByteChannel.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2018 Sober Lemur S.a.s. di Vacondio Andrea
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,14 +28,17 @@ public class DevNullWritableByteChannel implements WritableByteChannel {
 
     private boolean open = true;
 
+    @Override
     public boolean isOpen() {
         return open;
     }
 
+    @Override
     public void close() {
         this.open = false;
     }
 
+    @Override
     public int write(ByteBuffer src) {
         return src.remaining();
     }
--- src/test/java/org/sejda/io/BufferedSeekableSourceTest.java
+++ src/test/java/org/sejda/io/BufferedSeekableSourceTest.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2018 Sober Lemur S.a.s. di Vacondio Andrea
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,9 +24,6 @@ import java.nio.ByteBuffer;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.sejda.io.BufferedSeekableSource;
-import org.sejda.io.ByteArraySeekableSource;
-import org.sejda.io.SeekableSource;
 
 /**
  * @author Andrea Vacondio
