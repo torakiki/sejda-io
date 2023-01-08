c6797b640543def8bfab4197fe9b5fb35e98e4f0
torakiki <andrea.vacondio@gmail.com>
2023-01-08 16:57:03 +0100

    Changed Company name

--- src/test/java/org/sejda/io/ByteArraySeekableSourceTest.java
+++ src/test/java/org/sejda/io/ByteArraySeekableSourceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Sober Lemur S.a.s. di Vacondio Andrea
+ * Copyright 2018 Sober Lemur S.r.l.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
